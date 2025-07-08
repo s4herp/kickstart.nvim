a1c082f481fa73f4d0dac257b762b1593f59bc0a
Saher <saher.pinero@mudango.com>
2025-07-08 18:57:39 -0400

    Fisrt custom configs

--- init.lua
+++ init.lua
@@ -91,13 +91,13 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
-
+vim.o.autoread = true -- Automatically reload files when changed outside of Neovim
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
@@ -199,6 +199,23 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- NAVEGACIÓN Y ARCHIVOS (usando letras libres)
+vim.keymap.set('n', '<leader>p', '<cmd>Telescope find_files<cr>', { desc = 'Find files ([P]roject files)' })
+vim.keymap.set('n', '<leader>P', '<cmd>Telescope find_files hidden=true<cr>', { desc = 'Find all files (including hidden)' })
+
+-- BUFFER MANAGEMENT (usando w para window/close)
+vim.keymap.set('n', '<leader>w', '<cmd>bdelete<cr>', { desc = 'Close buffer ([W]indow close)' })
+vim.keymap.set('n', '<leader>W', '<cmd>bdelete!<cr>', { desc = 'Force close buffer' })
+vim.keymap.set('n', '<leader>n', '<cmd>enew<cr>', { desc = '[N]ew buffer' })
+vim.keymap.set('n', '<leader>o', '<cmd>%bd|e#<cr>', { desc = 'Close [O]ther buffers' })
+vim.keymap.set('n', '<leader>Q', '<cmd>qa<cr>', { desc = '[Q]uit all' })
+-- Navegación entre buffers
+vim.keymap.set('n', '<leader>>', '<cmd>bnext<cr>', { desc = 'Next buffer' })
+vim.keymap.set('n', '<leader><', '<cmd>bprev<cr>', { desc = 'Previous buffer' })
+
+-- EDICIÓN (usando letras que no están en uso)
+vim.keymap.set('n', '<leader>a', 'ggVG', { desc = 'Select [A]ll' })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
@@ -230,6 +247,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 
+vim.g.python3_host_prog = '/Users/saherpinero/.pyenv/versions/3.10.14/bin/python3'
+vim.g.ruby_host_prog = '/Users/saherpinero/.rbenv/versions/3.1.6/bin/ruby'
+
+vim.g.loaded_perl_provider = 0
+
 ---@type vim.Option
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
@@ -248,7 +270,7 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-
+  'github/copilot.vim',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -282,6 +304,108 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
     },
+  },
+
+  {
+    'greggh/claude-code.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      require('claude-code').setup()
+    end,
+  },
+
+  -- ========== SOLO BUFFERLINE (PESTAÑAS) ==========
+
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    event = 'VeryLazy',
+    keys = {
+      -- Gestión de pestañas
+      { '<leader>bp', '<cmd>BufferLineTogglePin<cr>', desc = 'Pin Buffer' },
+      { '<leader>bP', '<cmd>BufferLineGroupClose ungrouped<cr>', desc = 'Close non-pinned buffers' },
+      { '<leader>br', '<cmd>BufferLineCloseRight<cr>', desc = 'Close buffers to the right' },
+      { '<leader>bl', '<cmd>BufferLineCloseLeft<cr>', desc = 'Close buffers to the left' },
+    },
+    opts = {
+      options = {
+        close_command = 'bdelete! %d',
+        right_mouse_command = 'bdelete! %d',
+        left_mouse_command = 'buffer %d',
+
+        -- Iconos y apariencia
+        indicator = {
+          icon = '▎',
+          style = 'icon',
+        },
+        buffer_close_icon = '󰅖',
+        modified_icon = '●',
+        close_icon = '',
+        left_trunc_marker = '',
+        right_trunc_marker = '',
+
+        -- Tamaños
+        max_name_length = 30,
+        max_prefix_length = 30,
+        tab_size = 21,
+
+        -- Diagnósticos en las pestañas
+        diagnostics = 'nvim_lsp',
+        diagnostics_update_in_insert = false,
+
+        -- SIN OFFSETS - esto es lo que causaba el problema
+        -- NO agregues offsets para neo-tree si no lo usas
+        -- offsets = {}, -- Comentado o removido
+
+        -- Apariencia
+        show_buffer_icons = true,
+        show_buffer_close_icons = true,
+        show_close_icon = true,
+        show_tab_indicators = true,
+        persist_buffer_sort = true,
+        separator_style = 'slant', -- 'slant', 'thick', 'thin', 'slope'
+        enforce_regular_tabs = true,
+        always_show_bufferline = true,
+
+        -- Hover
+        hover = {
+          enabled = true,
+          delay = 200,
+          reveal = { 'close' },
+        },
+      },
+    },
+  },
+
+  {
+    'rafamadriz/friendly-snippets',
+    config = function()
+      require('luasnip.loaders.from_vscode').lazy_load()
+    end,
+  },
+
+  {
+    'vim-test/vim-test',
+    dependencies = { 'tpope/vim-dispatch' },
+    keys = {
+      { '<leader>tt', '<cmd>TestNearest<CR>', desc = 'Run nearest test' },
+      { '<leader>tf', '<cmd>TestFile<CR>', desc = 'Run current test file' },
+      { '<leader>ta', '<cmd>TestSuite<CR>', desc = 'Run all tests' },
+      { '<leader>tl', '<cmd>TestLast<CR>', desc = 'Run last test' },
+    },
+    config = function()
+      vim.g['test#strategy'] = 'dispatch' -- o 'neovim' para usar terminal
+      vim.g['test#ruby#use_bundler'] = 1
+      vim.g['test#ruby#rails#executable'] = 'bin/rails'
+    end,
+  },
+
+  {
+    'tpope/vim-rails',
+    event = 'BufReadPre',
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
@@ -351,6 +475,168 @@ require('lazy').setup({
     },
   },
 
+  {
+    'lewis6991/gitsigns.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = {
+      signs = {
+        add = { text = '┃' },
+        change = { text = '┃' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      signs_staged = {
+        add = { text = '┃' },
+        change = { text = '┃' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      signs_staged_enable = true,
+      signcolumn = true,
+      numhl = false,
+      linehl = false,
+      word_diff = false,
+      watch_gitdir = {
+        follow_files = true,
+      },
+      auto_attach = true,
+      attach_to_untracked = false,
+      current_line_blame = false, -- Toggle con :Gitsigns toggle_current_line_blame
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol',
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
+      preview_config = {
+        border = 'rounded',
+        style = 'minimal',
+        relative = 'cursor',
+        row = 0,
+        col = 1,
+      },
+      on_attach = function(bufnr)
+        local gitsigns = require 'gitsigns'
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navegación
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk 'next'
+          end
+        end, { desc = 'Next Git hunk' })
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk 'prev'
+          end
+        end, { desc = 'Previous Git hunk' })
+
+        -- Actions
+        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Stage hunk' })
+        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Reset hunk' })
+        map('v', '<leader>hs', function()
+          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'Stage hunk' })
+        map('v', '<leader>hr', function()
+          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'Reset hunk' })
+        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Stage buffer' })
+        map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'Undo stage hunk' })
+        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'Reset buffer' })
+        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'Preview hunk' })
+        map('n', '<leader>hb', function()
+          gitsigns.blame_line { full = true }
+        end, { desc = 'Blame line' })
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'Toggle line blame' })
+        map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Diff this' })
+        map('n', '<leader>hD', function()
+          gitsigns.diffthis '~'
+        end, { desc = 'Diff this ~' })
+
+        -- Text object
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
+      end,
+    },
+  },
+
+  -- 2. DIFFVIEW - Para visualización de diferencias
+  {
+    'sindrets/diffview.nvim',
+    dependencies = 'nvim-lua/plenary.nvim',
+    cmd = {
+      'DiffviewOpen',
+      'DiffviewClose',
+      'DiffviewToggleFiles',
+      'DiffviewFocusFiles',
+      'DiffviewRefresh',
+      'DiffviewFileHistory',
+    },
+    keys = {
+      { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = 'Git Diff View' },
+      { '<leader>gh', '<cmd>DiffviewFileHistory<cr>', desc = 'Git File History' },
+      { '<leader>gf', '<cmd>DiffviewFileHistory %<cr>', desc = 'Current File History' },
+      { '<leader>gc', '<cmd>DiffviewClose<cr>', desc = 'Close Diff View' },
+    },
+    opts = {},
+  },
+
+  -- 3. NEOGIT - Interface Git completa (CONFIGURACIÓN ACTUALIZADA)
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    cmd = 'Neogit',
+    keys = {
+      { '<leader>gs', '<cmd>Neogit<cr>', desc = 'Git Status' },
+      { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Git Status' },
+    },
+    config = function()
+      require('neogit').setup {
+        -- Configuración básica
+        disable_hint = false,
+        disable_context_highlighting = false,
+        disable_signs = false,
+
+        -- Auto refresh
+        auto_refresh = true,
+
+        -- Sorting
+        sort_branches = '-committerdate',
+
+        -- Configuración de apertura
+        kind = 'tab',
+
+        -- Integración con diffview
+        integrations = {
+          diffview = true,
+          telescope = true,
+        },
+
+        -- Console timeout
+        console_timeout = 2000,
+        auto_show_console = true,
+      }
+    end,
+  },
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
@@ -698,6 +984,18 @@ require('lazy').setup({
             },
           },
         },
+
+        solargraph = {
+          cmd = { '/Users/saherpinero/.rbenv/shims/solargraph', 'stdio' },
+          settings = {
+            solargraph = {
+              diagnostics = true,
+              formatting = true,
+              useBundler = false,
+              completion = true,
+            },
+          },
+        },
       }
 
       -- Ensure the servers and tools above are installed
@@ -1014,3 +1312,4 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
