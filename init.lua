1c0da9c1eedbcd41fc7622242d3df9a36dac75ef
Saher <saher.pinero@mudango.com>
2025-07-10 08:59:08 -0400

    Add vim-coach plugin

--- init.lua
+++ init.lua
@@ -197,6 +197,18 @@ require('lazy').setup({
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
+  {
+    "shahshlok/vim-coach.nvim",
+    dependencies = {
+      "folke/snacks.nvim",
+    },
+    config = function()
+      require("vim-coach").setup()
+    end,
+    keys = {
+      { "<leader>?", "<cmd>VimCoach<cr>", desc = "Vim Coach" },
+    },
+  },
   -- ========== SOLO BUFFERLINE (PESTAÑAS) ==========
 
   {
@@ -1353,7 +1365,7 @@ require('lazy').setup({
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
         -- Disable for large files to improve performance
-        disable = function(lang, buf)
+        disable = function(_lang, buf)
           local max_filesize = 100 * 1024 -- 100 KB
           local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
