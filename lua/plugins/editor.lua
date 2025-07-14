33edf3ec020f16d503905fe19d2ba75b9af8a321
Saher <saher.pinero@mudango.com>
2025-07-13 20:14:01 -0400

    Improve motion by adding Leap plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -238,7 +238,25 @@ return {
       end, { desc = '[R]ails grep [t]ests' })
     end,
   },
-
+  -- Leap (motion plugin)
+  {
+    'ggandor/leap.nvim',
+    enabled = true,
+    keys = {
+      { 's', mode = { 'n', 'x', 'o' }, desc = 'Leap Forward to' },
+      { 'S', mode = { 'n', 'x', 'o' }, desc = 'Leap Backward to' },
+      { 'gs', mode = { 'n', 'x', 'o' }, desc = 'Leap from Windows' },
+    },
+    config = function(_, opts)
+      local leap = require 'leap'
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      vim.keymap.del({ 'x', 'o' }, 'x')
+      vim.keymap.del({ 'x', 'o' }, 'X')
+    end,
+  },
   -- Treesitter (syntax highlighting)
   {
     'nvim-treesitter/nvim-treesitter',
@@ -436,7 +454,7 @@ return {
   {
     'NMAC427/guess-indent.nvim',
     config = function()
-      require('guess-indent').setup({})
+      require('guess-indent').setup {}
     end,
   },
 }
