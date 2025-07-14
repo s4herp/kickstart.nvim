b3a2d4abc8d843d0668f67ae334b03198afaa28a
Saher <saher.pinero@mudango.com>
2025-07-13 23:18:19 -0400

    Replace leap.nvim with flash.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -238,24 +238,52 @@ return {
       end, { desc = '[R]ails grep [t]ests' })
     end,
   },
-  -- Leap (motion plugin)
   {
-    'ggandor/leap.nvim',
-    enabled = true,
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = {},
     keys = {
-      { 's', mode = { 'n', 'x', 'o' }, desc = 'Leap Forward to' },
-      { 'S', mode = { 'n', 'x', 'o' }, desc = 'Leap Backward to' },
-      { 'gs', mode = { 'n', 'x', 'o' }, desc = 'Leap from Windows' },
+      {
+        's',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash',
+      },
+      {
+        'S',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').treesitter()
+        end,
+        desc = 'Flash Treesitter',
+      },
+      {
+        'r',
+        mode = 'o',
+        function()
+          require('flash').remote()
+        end,
+        desc = 'Remote Flash',
+      },
+      {
+        'R',
+        mode = { 'o', 'x' },
+        function()
+          require('flash').treesitter_search()
+        end,
+        desc = 'Treesitter Search',
+      },
+      {
+        '<c-s>',
+        mode = { 'c' },
+        function()
+          require('flash').toggle()
+        end,
+        desc = 'Toggle Flash Search',
+      },
     },
-    config = function(_, opts)
-      local leap = require 'leap'
-      for k, v in pairs(opts) do
-        leap.opts[k] = v
-      end
-      leap.add_default_mappings(true)
-      vim.keymap.del({ 'x', 'o' }, 'x')
-      vim.keymap.del({ 'x', 'o' }, 'X')
-    end,
   },
   -- Treesitter (syntax highlighting)
   {
